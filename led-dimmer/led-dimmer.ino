e1e24b18e3b102ab86e1b3ca5d895de505d1aff1
Tilman Adler <tilman-git@t-animal.de>
2022-04-29 20:18:02 +0200

    Add API for setting the current dimming value

--- led-dimmer/led-dimmer.ino
+++ led-dimmer/led-dimmer.ino
@@ -1,32 +1,22 @@
+#include "string"
+
 #include <WiFiManager.h>
 #include <ESP8266WebServer.h>
 
+#define PWM_PIN D4
 
 ESP8266WebServer server(80);
 
-void httpServerHandleRoot() {
-  server.send(200, "text/plain", "Hallo Welt");
-}
+void httpServerHandleRoot();
+void httpServerHandleNotFound();
+void httpServerHandleDimValue();
 
-void httpServerHandleNotFound() {
-  String message = "File Not Found\n\n";
-  message += "URI: ";
-  message += server.uri();
-  message += "\nMethod: ";
-  message += (server.method() == HTTP_GET) ? "GET" : "POST";
-  message += "\nArguments: ";
-  message += server.args();
-  message += "\n";
-  for (uint8_t i = 0; i < server.args(); i++) {
-  message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
-  }
-  server.send(404, "text/plain", message);
-}
+unsigned int currentPercentage = 0;
 
-void setup() {
+const char* contentType = "Content-Type";
+
+void setUpAndConnectWifi() {
   WiFi.mode(WIFI_STA); // explicitly set mode, esp defaults to STA+AP
- 
-  Serial.begin(115200);
   
   WiFiManager wm;
 
@@ -45,12 +35,99 @@ void setup() {
   else {
     Serial.println("Successfully connected to Wifi)");
   }
+}
+
+void setup() {
+  setUpAndConnectWifi();
+ 
+  Serial.begin(115200);
+
+  pinMode(PWM_PIN, OUTPUT);
 
   server.on("/", httpServerHandleRoot);
+  server.on("/current", httpServerHandleDimValue);
   server.onNotFound(httpServerHandleNotFound);
+  
+  server.collectHeaders(&contentType, 1);
   server.begin();
 }
 
 void loop() {
   server.handleClient();
+  analogWrite(PWM_PIN, (100 - currentPercentage) * 1023 / 100); 
+}
+
+void sendUserError(String text) {
+  server.send(400, "text/plain", text);
+}
+
+void sendSuccess(String text) {
+  server.send(200, "text/plain", text);
+}
+
+void sendCurrentPercentage() {
+  sendSuccess(String(currentPercentage));
+}
+
+void httpServerHandleDimValue() {
+  if(server.method() == HTTP_GET) {
+    sendCurrentPercentage();
+    return;
+  }
+
+  if(server.method() == HTTP_POST) {
+    String body = server.arg("plain");
+
+    if(server.header(contentType) != "text/plain") {
+      sendUserError("Send header Content-Type: text/plain");
+      return;
+    }
+
+    if(body.length() > 3 || body.length() == 0) {
+      sendUserError("Input too short or too long");
+      return;
+    }
+
+    for (size_t i = 0; i < body.length(); i++) {
+      if(!isDigit(body[i])) {
+        sendUserError("Malformatted input at char " + i);
+        return;
+      }
+    }
+
+    int newValue = body.toInt();
+    
+    if(newValue < 0 || newValue > 100) {
+      sendUserError("Out of range (must be -1 < x < 101)");
+      return;
+    }
+    
+    currentPercentage = newValue;
+    
+    sendCurrentPercentage();
+    return;
+  }
+
+  server.send(405, "text/plain", "Method Not Allowed");
+}
+
+void httpServerHandleRoot() {
+  server.send(200, "text/plain", "Hallo Welt");
+}
+
+void httpServerHandleNotFound() {
+  String message = "File Not Found\n\n";
+  message += "URI: ";
+  message += server.uri();
+  message += "\nMethod: ";
+  message += (server.method() == HTTP_GET) ? "GET" : "POST";
+  message += "\nContent-Type: " + server.headers() + server.header(contentType);
+  message += "\nArguments: ";
+  message += server.args();
+  message += "\n";
+  for (uint8_t i = 0; i < server.args(); i++) {
+  message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
+  }
+  message += "body: " + server.arg("plain");
+  server.send(404, "text/plain", message);
 }
