603ddc10b199a9f0b832bde540d305aceab6338a
Tilman Adler <tilman-git@t-animal.de>
2022-06-11 13:53:59 +0200

    minify html ,too

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,12 @@
 import { defineConfig } from "vite"
 import { viteSingleFile } from "vite-plugin-singlefile"
+import { createHtmlPlugin } from "vite-plugin-html"
 
 export default defineConfig({
 	plugins: [
+		createHtmlPlugin({
+			minify: true,
+		}),
 		viteSingleFile({ removeViteModuleLoader: true })
 	],
 })
